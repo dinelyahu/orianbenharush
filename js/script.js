52b922663c082b7fd480ecd59e40933088d45933
dinelyahu <dinelyahu@gmail.com>
2025-03-10 18:17:34 +0200

    סכהנגס

--- js/script.js
+++ js/script.js
@@ -504,3 +504,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("preloader").classList.add("preloader-hidden");
+        setTimeout(function () {
+            document.getElementById("preloader").style.display = "none";
+        }, 1000); // נוודא שהוא מוסר מה-DOM אחרי האנימציה
+    }, 2000); // מסך הפתיחה ייעלם אחרי 2 שניות
+});
