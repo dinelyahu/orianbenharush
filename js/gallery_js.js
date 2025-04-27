59b2dd4f116f3a6d81f6137699432220a25d46f4
dinelyahu <dinelyahu@gmail.com>
2025-04-27 18:01:28 +0300

    fxb

--- js/gallery_js.js
+++ js/gallery_js.js
@@ -318,3 +318,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const backToTopButton = document.getElementById("back-to-top");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) { // מופיע אחרי גלילה של 300 פיקסלים
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
